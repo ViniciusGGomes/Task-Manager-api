b8817df5e5a03a505721a9524861422d77d6533e
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-06-16 22:16:04 +0000

    feat: create custom AppError Class for error handling

---
+++ src/utils/AppError.ts
@@ -0,0 +1,11 @@
+class AppError {
+  message: string;
+  status_code: number;
+
+  constructor(message: string, status_code: number = 400) {
+    this.message = message;
+    this.status_code = status_code;
+  }
+}
+
+export { AppError };
